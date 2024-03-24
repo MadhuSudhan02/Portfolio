e4f672fc9776c8d4fe11cef9fafd0d318ff3e960
Madhu Sudhan <m02sudhan@gmail.com>
2024-03-24 16:28:27 +0530

    my portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener  } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Education, experienceData, technicalSkills } from './constant/portfolio-constant';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'portfolioProject';
+  experienceList : any = experienceData;
+  technicalSkillsList: any = technicalSkills;
+  educationList : any = Education;
+  activeSection: string = 'home';
+  constructor(private router: Router) { }
+  ngOnInit(): void {
+    this.activeSection = 'home';
+    console.log(this.experienceList);
+  }
+
+  setActiveSection(sectionId: string): void {
+    this.activeSection = sectionId;
+    let e: any = document.getElementById(sectionId);
+    e.scrollIntoView({
+      block: 'start',
+      behavior: 'smooth',
+      inline: 'start'
+    });
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    const scrollPosition = window.scrollY;
+    const windowHeight = window.innerHeight;
+
+    // Get all elements with an ID starting with 'section'
+    const sections = document.querySelectorAll('[id="experience"], [id="skills"], [id="education"], [id="home"]');
+  
+    
+   // Find the first section that is at least partially in view
+   let inViewSectionId = '';
+   sections.forEach((section: any) => {
+     const rect = section.getBoundingClientRect();
+     if ((rect.top >= 0 && rect.top <= windowHeight) || (rect.bottom >= 0 && rect.bottom <= windowHeight)) {
+       inViewSectionId = section.id;
+       return; // Exit the loop after finding the first visible section
+     }
+   });
+
+    // Update the fragment identifier in the URL
+    if (inViewSectionId) {
+      this.activeSection = inViewSectionId;
+      this.router.navigateByUrl(`/#${inViewSectionId}`);
+    }
+  }
+
 }
---
+++ src/app/constant/portfolio-constant.ts
@@ -0,0 +1,137 @@
+// technical skills and experience
+export const experienceData = [{
+    "companyName": "Bridge Labz(Client- Tata Communication Pvt Ltd)",
+    "jobRole": "Software Engineer (24-Apr-2023 - current)",
+    "project": [
+        {
+            "projectName": "CloudLyte Proximate",
+            "projectExperience": "Tata Communications CloudLyte is an managed platform that gives you the flexibility of securely deploying and managing any application anywhere at scale. CloudLyte Proximate, provides seamless on-premises edge-in-a-box solution to run ultra-low latency use cases.",
+            "technicalSkills": "Angular, TypeScript, Postman, Jira, Jenkins",
+        },
+        {
+            "projectName": "BulkNde",
+            "projectExperience": "bulk nde is the NDE project . the main goal of the project is to establish multiple site",
+            "technicalSkills": "Angular, TypeScript, MySql, Postman, Jira, Jenkins",
+        },
+        {
+            "projectName": "TEMPLATE COF(Customer Order Form)",
+            "projectExperience": "bulk nde is the NDE project . the main goal of the project is to establish multiple site",
+            "technicalSkills": "Angular, TypeScript, Postman",
+        }
+    ]
+
+},
+{
+    "companyName": "Free Lance",
+    "jobRole": "Designer and Developer",
+    "project": [{
+        "projectName": "TechMinds",
+        "projectExperience": "Experienced Customer Relations Officer adept at providing technical support to Airtel customers, specifically troubleshooting router issues, raising tickets, and coordinating with engineers for resolution. Consistently recognized for top-ranking performance nationwide due to diligent problem-solving and customer-centric approach. Seeking to leverage this expertise to deliver outstanding customer experiences in a challenging environment.",
+        "technicalSkills": "Figma, Wireframe, UI design, HTML5, CSS, JavaScript",
+        "link": "https://github.com/kiranfranklin999/Techminds_webpage"
+    }]
+},
+{
+    "companyName": "Altruist Technology Pvt Lmt",
+    "jobRole": "Customer Relations Officer (21-Jun-22 to 16-Mar-23)",
+    "project": [{
+        "projectName": "Airtel Broadband ",
+        "projectExperience": "Experienced Customer Relations Officer adept at providing technical support to Airtel customers, specifically troubleshooting router issues, raising tickets, and coordinating with engineers for resolution. Consistently recognized for top-ranking performance nationwide due to diligent problem-solving and customer-centric approach. Seeking to leverage this expertise to deliver outstanding customer experiences in a challenging environment.",
+        "technicalSkills": "Patience and Resilience, Communication Skills, Interpersonal Skills, Problem-Solving Skills, Computer Literacy",
+    }]
+}
+];
+
+export const technicalSkills = [
+    {
+        'skillName': "Angular Framework",
+        "skillDescription": "Proficient in Angular Framework, HTML5, CSS, JavaScript, and TypeScript: I possess extensive experience in developing robust web applications using Angular framework and related technologies."
+    },
+    {
+        'skillName': "Version Control",
+        "skillDescription": "Experienced in version control with Git, GitLab, and GitHub, as well as using SourceTree for repository management: I am adept at managing codebase effectively using version control systems."
+    },
+    {
+        'skillName': " Browser Developer Tools",
+        "skillDescription": "Skilled in analyzing HTML/CSS elements, debugging JavaScript code, and testing responsive design to ensure optimal performance and a seamless user experience across various browsers and devices."
+    },
+    {
+        'skillName': "API Testing and Documentation",
+        "skillDescription": "Skilled in API testing and documentation using Postman: I have experience in testing and documenting APIs to ensure their functionality and reliability."
+    },
+    {
+        'skillName': "Project Management Tools",
+        "skillDescription": "Proficient in project management tools such as Jira: I am proficient in using Jira for project management and collaboration."
+    },
+    {
+        'skillName': "Basic Java and MySql",
+        "skillDescription": "Basic understanding of Java programming language: I possess foundational knowledge of Java programming language, allowing me to understand and collaborate effectively with backend teams."
+    },
+    
+    {
+        'skillName': "Continuous Integration/Continuous Deployment (CI/CD)",
+        "skillDescription": "Familiar with continuous integration/continuous deployment (CI/CD) processes with Jenkins: I have a solid understanding of CI/CD processes and their role in ensuring project efficiency."
+    },
+    {
+        'skillName': "UI/UX Principles",
+        "skillDescription": "By adhering to UI/UX principles, my aim is to create intuitive, efficient, and enjoyable digital experiences for users across various platforms and devices."
+    },
+    
+];
+
+export const Education = [
+    {
+        "academy" : "BridgeLabz",
+        "course" : "Angular Framework, Git, Basic Java, MySql",
+        "project" : [
+            {
+                "projectName" : "BookStore",
+                "description" : "This is an e-commerce website, where the admin can add/remove the book and set the price, where the customer use to buy them. The swagger was given to me for the API calls. I embrace my skill with User Authetication, Routing and many more.",
+                "link" : "https://github.com/MadhuSudhan02/BookStore"
+            },
+            {
+                "projectName" : "Discovery Land Company",
+                "description" : "The Discovery Land Company was an gallery page, where the user can view the images of the paticular place. Here the flex property was very important so thè website is responsive",
+                "link" : "https://github.com/MadhuSudhan02/DiscoveryLandCompany"
+            },
+            {
+                "projectName" : "Fundoo",
+                "description" : "This is the best project I have worked on. It is google keep features implemention that user can had notes and perform rest call. It was very challenging because it had many features where I was able to explore Angular Material",
+                "link" : "https://github.com/MadhuSudhan02/Fundoo"
+            },
+        ]
+    },
+    {
+        "academy" : "Bridge UX",
+        "course" : "Advanced User Experience Design for Web & Mobile",
+        "project" : [
+            {
+                "projectName" : "Mcare",
+                "description" : "Mcare is a doctor consultation app designed to provide convenient access to medical advice and services. Users can easily connect with healthcare professionals, schedule appointments, and access their medical records. The app prioritizes user-friendly navigation and secure communication for a seamless and efficient healthcare experience.",
+                "link" : "https://www.figma.com/file/oYyqxNBY4WhW7N4gXYRZot/Mcare-project?type=design&node-id=0-1&mode=design&t=Bvz9GkTWOyeBXdSA-0"
+            },
+            {
+                "projectName" : "Chatbot",
+                "description" : "This UI design is for a chatbot, providing a user-friendly interface for interacting with an AI-powered assistant. It offers intuitive messaging features and visual cues for seamless communication and task completion. The design prioritizes simplicity and efficiency, enhancing the overall user experience in engaging with the chatbot.",
+                "link" : "https://www.figma.com/file/X9cEkFqvo5FdmL3HNZppZf/Chatbox?type=design&mode=design&t=JGhTGcj9G5NqXqMS-0"
+            },
+            {
+                "projectName" : "IOT Project",
+                "description" : "This UI design is for an IoT project, facilitating remote control and monitoring of connected devices. It prioritizes user-friendliness, offering intuitive navigation and visual feedback for efficient device management within the Internet of Things ecosystem.",
+                "link" : "https://www.figma.com/file/yGuyBEl652gIB9TfuqgAxT/IOT-project?type=design&mode=design&t=JGhTGcj9G5NqXqMS-0"
+            },
+        ]
+    },
+    {
+        "academy" : "Dayananda Sagar College of Engineering",
+        "course" : "Electronic and Communication Engineering",
+        "project" : [
+            {
+                "projectName" : "Biometric Smart Door Lock System",
+                "description" : "A Biometric Smart Door Lock uses unique biological traits like fingerprints or facial recognition for access, enhancing security and convenience. It eliminates the need for keys or codes, providing a modern and reliable access control solution for homes and businesses.",
+                
+            }
+        ]
+    },
+]
+
